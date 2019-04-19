5ec49b58e96126ff7e5978e1f4ec28b1b87441bb
Akshaye Anitha Pradeep <akshayeanithapradeep@Akshayes-MacBook-Pro.local>
2019-04-19 13:55:25 -0500

    Added image rec

--- app/server.py
+++ app/server.py
@@ -4,40 +4,25 @@ from starlette.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn, aiohttp, asyncio
 from io import BytesIO
+from keras.preprocessing import image
+import matplotlib
+import matplotlib.pyplot as plt
+import cv2
+from keras.models import model_from_json
 
-from fastai import *
-from fastai.vision import *
-
-# export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
-
-classes = ['black', 'grizzly', 'teddys']
+classes = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 path = Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
-async def download_file(url, dest):
-    if dest.exists(): return
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f: f.write(data)
-
 async def setup_learner():
-    await download_file(export_file_url, path/export_file_name)
-    try:
-        learn = load_learner(path, export_file_name)
-        return learn
-    except RuntimeError as e:
-        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-            print(e)
-            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-            raise RuntimeError(message)
-        else:
-            raise
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    model = model_from_json(open("facial_expression_model_structure.json", "r").read())
+    model.load_weights('facial_expression_model_weights.h5')  # load weights
+
+
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
@@ -46,16 +31,38 @@ loop.close()
 
 @app.route('/')
 def index(request):
-    html = path/'view'/'index.html'
+    html = path/'index.html'
     return HTMLResponse(html.open().read())
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
     data = await request.form()
-    img_bytes = await (data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+#    img_bytes = await (data['file'].read())
+    img = cv2.imread(data['file'])
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # print(faces) #locations of detected faces
+    count = 0
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)  # draw rectangle to main image
+
+        detected_face = img[int(y):int(y + h), int(x):int(x + w)]  # crop detected face
+        detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)  # transform to gray scale
+        detected_face = cv2.resize(detected_face, (48, 48))  # resize to 48x48
+
+        img_pixels = image.img_to_array(detected_face)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+
+        img_pixels /= 255  # pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
+        predictions = model.predict(img_pixels)  # store probabilities of 7 expressions
+        break;
+#    img = open_image(BytesIO(img_bytes))
+#    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(predictions)})
+
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
