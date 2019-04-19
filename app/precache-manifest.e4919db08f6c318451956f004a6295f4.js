3417ea431192e78075608c9ab32a7f9ddb20d3b5
Akshaye Anitha Pradeep <akshayeanithapradeep@Akshayes-MacBook-Pro.local>
2019-04-19 13:28:00 -0500

    Added gui

---
+++ app/precache-manifest.e4919db08f6c318451956f004a6295f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68b1094953c842b1c796",
+    "url": "/static/css/main.80693893.chunk.css"
+  },
+  {
+    "revision": "68b1094953c842b1c796",
+    "url": "/static/js/main.fed86bee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "98788340a0cd82529295",
+    "url": "/static/js/2.8f61f45b.chunk.js"
+  },
+  {
+    "revision": "e678a9d9c99c2dac9ef97bc15c724240",
+    "url": "/index.html"
+  }
+];
