c8c296da8622ebe83512ea26cbcd72491081c955
Akshaye Anitha Pradeep <akshayeanithapradeep@client-10-228-241-103.tamulink.tamu.edu>
2019-04-22 18:55:50 -0500

    Added recogntion

--- app/server.py
+++ app/server.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import cv2
 from keras.models import model_from_json
 import sys
-
 from fastai import *
 from fastai.vision import *
 
@@ -42,20 +41,23 @@ def index(request):
 async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
-#    img = cv2.imread(data['file'])
-    img = open_image(BytesIO(img_bytes))
-    print(img.shape)
-    img = image2np(img)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    img = open_image(BytesIO(img_bytes), , convert_mode='L', div=False)
+    img1 = image2np(img.data)
+    img1.shape
+    img1 = np.expand_dims(img1, axis = 2)
+    img1 = np.concatenate((img1, img1, img1), axis = 2)
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    model = model_from_json(open("facial_expression_model_structure.json", "r").read())
+    model.load_weights('facial_expression_model_weights.h5')  # load weights
+    gray = gray.astype(np.uint8)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    print(faces) #locations of detected faces
     count = 0
-
+    emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    emotion = None
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)  # draw rectangle to main image
+        cv2.rectangle(img1, (x, y), (x + w, y + h), (255, 0, 0), 2)  # draw rectangle to main image
 
-        detected_face = img[int(y):int(y + h), int(x):int(x + w)]  # crop detected face
+        detected_face = img1[int(y):int(y + h), int(x):int(x + w)]  # crop detected face
         detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)  # transform to gray scale
         detected_face = cv2.resize(detected_face, (48, 48))  # resize to 48x48
 
@@ -65,10 +67,12 @@ async def analyze(request):
 
         img_pixels /= 255  # pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
         predictions = model.predict(img_pixels)  # store probabilities of 7 expressions
+        #find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
+        max_index = np.argmax(predictions[0])
+
+        emotion = emotions[max_index]
         break;
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(predictions)})
+    return JSONResponse({'result': emotion})
 
 
 if __name__ == '__main__':
